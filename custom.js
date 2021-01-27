f4b04bca1e3a1fa9c0a1a9c6d1b3de0b43b160b5
rkdudley <rattikarndudley@gmail.com>
2021-01-27 10:27:41 -0500

    delete hiding option

--- custom.js
+++ custom.js
@@ -1,7 +1,19 @@
 //display photos
 
-let rock = document.getElementById("randomPick");
+let rockCom = document.getElementById("randomPick");
 
 function showIt(){
  document.getElementById("rockCom").style.visibility = "visible";
 }
+
+//user side
+let rockUser = document.getElementById("rockUser");
+let paperUser = document.getElementById("paperUser");
+let scissorUser = document.getElementById("scissorUser");
+
+
+function choiceDisplay(imgSrc){
+   var imageLocation = document.getElementById("img");
+   imageLocation.src = imgSrc;
+}
+
