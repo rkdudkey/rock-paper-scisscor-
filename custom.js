cd45b96dded21bf0e0643f2111f11bf601844372
rkdudley <rattikarndudley@gmail.com>
2021-01-27 11:35:00 -0500

    add function random picture

--- custom.js
+++ custom.js
@@ -1,19 +1,14 @@
-//display photos
-
-let rockCom = document.getElementById("randomPick");
-
-function showIt(){
- document.getElementById("rockCom").style.visibility = "visible";
-}
-
-//user side
-let rockUser = document.getElementById("rockUser");
-let paperUser = document.getElementById("paperUser");
-let scissorUser = document.getElementById("scissorUser");
-
-
+//display user choices
 function choiceDisplay(imgSrc){
    var imageLocation = document.getElementById("img");
    imageLocation.src = imgSrc;
 }
 
+
+function getRandomImages(){
+   //random pick choices display
+   var num = Math.floor(( Math.random() * 3) + 1);
+   var imageLocation = document.getElementById("img-opponent");
+   imageLocation.src = `images/games${num}.png`;
+   console.log(imageLocation); 
+}
